af169aa77fa72ec2699187d25d9c2157f7b494a7
Venek84 <dolezelv@post.cz>
2025-12-05 12:44:59 +0100

    Update main_worker.py

--- elan2mqtt/main_worker.py
+++ elan2mqtt/main_worker.py
@@ -819,7 +819,10 @@ async def main():
 
     # setup mqtt 
     mqtt.Client.connected_flag = False
-    mqtt_cli = mqtt.Client("eLan2MQTT_main_worker" + args.mqtt_id)
+    mqtt_cli = mqtt.Client(
+        client_id="eLan2MQTT_main_worker" + args.mqtt_id,
+        callback_api_version=5
+    )
     logger.info("Connecting to MQTT broker")
     logger.info(args.mqtt_broker)
 
